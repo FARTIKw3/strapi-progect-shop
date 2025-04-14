98e12d33cf11bbc5a441fe8454c3a2681e9848f3
Ramazan <ramazandjaparov@gmail.com>
2025-04-14 09:51:30 +0600

    .

---
+++ src/api/laptop/routes/laptop.ts
@@ -0,0 +1,7 @@
+/**
+ * laptop router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::laptop.laptop');
---
+++ src/api/laptop/services/laptop.ts
@@ -0,0 +1,7 @@
+/**
+ * laptop service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::laptop.laptop');
